01669e3ba97769e7d61e6a24ef77fee72217fc79
Ken <120130815+Moucchi@users.noreply.github.com>
2023-07-11 10:53:30 +0400

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,8 +1,15 @@
 CREATE TABLE code(
-    code_id INT PRIMARY KEY,
-    code INT NOT NULL,
+    code_id SERIAL PRIMARY KEY,
+    code INT(4) NOT NULL,
     value DOUBLE PRECISION NOT NULL
 );
+
+INSERT INTO code(code , value) VALUES (1000 , 1000) ,
+(1001 , 2000) , (1011 , 5000) ,  (1111 , 9000) ,
+(1201 , 10000) , (1979 , 67000) ,(2000 , 2000) , 
+(2075 , 28000) , (1999 , 73000) , (0541 , 33000) , 
+(4655 , 48000) , (0010 , 3000) , (1010 , 4000) , 
+(0110 , 7000) , (2011 , 6000) ;  
 
 CREATE TABLE usedCode(
     code_id INT ,
